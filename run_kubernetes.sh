a4300745da4faa79b64e8761b45d6eeace172c30
yahyalabanieh <yahyalab@gmail.com>
2019-12-27 03:33:55 +0200

    adjust kuberbetes shell file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ylabanieh/prediction_microservices"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker logic
+kubectl run prediction_microservices --image $dockerpath:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward deployment/prediction_microservices 8000:80
