e8569766a60f7bf1b33a361259631a11610ce563
yahyalabanieh <yahyalab@gmail.com>
2019-12-27 03:11:29 +0200

    adjust upload docker shell

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ylabanieh/prediction_microservices"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag prediction_microservices:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
